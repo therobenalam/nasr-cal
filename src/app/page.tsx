dafd510902c7e319c99a340132e3937f314205a6
roben alam <alalam.roben@gmail.com>
2025-01-25 12:19:28 +0200

    ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,138 @@
-import Image from "next/image";
+'use client';
+import Head from 'next/head';
+import React, { useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // State to store input data
+  const [hightData, setHightData] = useState('');
+  const [widthData, setWidthData] = useState('');
+  const [resultTableData, setResultTableData] = useState([{ id: '', key: '', val: 0 }]);
+  // State to toggle between input and result view
+  const [showResult, setShowResult] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+  // Handle input change
+  const handleHightChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHightData(e.target.value);
+  };
+
+  const handleWidthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setWidthData(e.target.value);
+  };
+
+  // Handle form submit
+  const handleSubmit = () => {
+    setResultTableData([
+      { id: '1', key: 'علو الحاجب', val: parseInt(hightData) + 5 },
+      { id: '2', key: 'علو الدرفة', val: parseInt(hightData) - 5.8 },
+      { id: '3', key: 'عرض السكة', val: parseInt(widthData) - 3.2 },
+      { id: '4', key: 'عرض الدرفة', val: (parseInt(widthData) - 3.2) / 2 },
+      { id: '5', key: 'علو اليو', val: parseInt(hightData) - 3.5 },
+      { id: '6', key: 'علو المنخل', val: parseInt(hightData) - 4 },
+      { id: '7', key: 'عرض المنخل', val: (parseInt(widthData) - 3.2) / 2 + 1.5 },
+    ]);
+    setShowResult(true);
+  };
+
+  // Handle reset
+  const handleReset = () => {
+    setHightData('');
+    setWidthData('');
+    setShowResult(false);
+  };
+
+  return (
+    <>
+    <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+    </Head>
+    <div style={styles.container}>
+      {!showResult ? (
+        <>
+          {/* Input Form */}
+          <input
+            style={styles.input}
+            value={hightData}
+            onChange={handleHightChange}
+            placeholder="طول"
+            type="number"
+            inputMode="numeric"
+            
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <input
+            style={styles.input}
+            value={widthData}
+            onChange={handleWidthChange}
+            placeholder="عرض"
+            type="number"
+            inputMode="numeric"
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <button style={styles.button} onClick={handleSubmit}>
+            Submit
+          </button>
+        </>
+      ) : (
+        <>
+          {/* Result Table */}
+          <ul style={styles.table}>
+            {resultTableData.map((item) => (
+              <li key={item.id} style={styles.tableRow}>
+                <span style={styles.tableCell}>{item.val}</span>
+                <span style={styles.tableCell}>:{item.key}</span>
+              </li>
+            ))}
+          </ul>
+          <button style={styles.button} onClick={handleReset}>
+            Reset
+          </button>
+        </>
+      )}
     </div>
+
+    </>
   );
 }
+
+const styles: { [key: string]: React.CSSProperties } = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '20px',
+    marginTop: '20%'
+  },
+  input: {
+    height: '40px',
+    color: 'black',
+    borderColor: 'gray',
+    borderWidth: '1px',
+    width: '100%',
+    marginBottom: '10px',
+    paddingRight: '10px',
+    fontSize: '16px',
+    direction: 'rtl'
+  },
+  button: {
+    padding: '10px 20px',
+    fontSize: '16px',
+    cursor: 'pointer',
+    border: '1px solid grey',
+    marginTop: '10px'
+  },
+  table: {
+    listStyleType: 'none',
+    padding: '0',
+    width: '100%',
+  },
+  tableRow: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '10px 0',
+    borderBottom: '1px solid #ccc',
+  },
+  tableCell: {
+    fontSize: '16px',
+  },
+};
